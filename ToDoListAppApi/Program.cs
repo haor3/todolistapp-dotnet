56ef1aa6bcbb0dfece2f4dc283312308610d5e3f
haor3 <iamhaobui@gmail.com>
2022-04-28 16:44:24 -0400

    commit

--- ToDoListAppApi/Program.cs
+++ ToDoListAppApi/Program.cs
@@ -1,8 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoListAppApi.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000", "https://to-do-list-app-haor3.vercel.app").AllowAnyMethod().AllowAnyHeader();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,10 +22,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ToDoListDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration.GetConnectionString("ConnectionString"));
 
 var app = builder.Build();
 
-app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
